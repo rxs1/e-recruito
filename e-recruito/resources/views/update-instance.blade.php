82d8c7d1de59a541dabab24ef57e936a907fb7e3
marsileacrenata <nadiyah.marsela@ui.ac.id>
2016-04-15 16:12:31 +0700

    Create update-instance.blade.php

---
+++ e-recruito/resources/views/update-instance.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	@include('update-instance-head')
+</head>
+<body>
+	@include('update-instance-content')
+</body>
+<footer>
+	@include('footer')
+</footer>
+</html>
