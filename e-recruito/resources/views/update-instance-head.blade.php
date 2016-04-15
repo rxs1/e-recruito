a2109f9f9694c62cb26872e66e8c56624c5302f6
marsileacrenata <nadiyah.marsela@ui.ac.id>
2016-04-15 16:15:00 +0700

    Create update-instance-head.blade.php

---
+++ e-recruito/resources/views/update-instance-head.blade.php
@@ -0,0 +1,6 @@
+@include('head')
+
+<link href='https://fonts.googleapis.com/css?family=Roboto:700' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Ubuntu:500italic' rel='stylesheet' type='text/css'>
+
+<link href="{{ asset('/public/assets/bootstrap/css/custom.css') }}" rel="stylesheet">
