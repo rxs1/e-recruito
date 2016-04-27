169a09a45d7109892ee5927f8e1e746b66231fd6
derhif <rhief.at@gmail.com>
2016-04-27 18:32:27 +0700

    Penyesuaian user management dengan database baru

--- e-recruito/resources/views/admin/users/edit.blade.php
+++ e-recruito/resources/views/admin/users/edit.blade.php
@@ -11,19 +11,27 @@
 				<div class="panel panel-primary">
 					<div class="panel-heading">Edit data User</div>
 					<div class="panel-body">
-						{!! Form::model($user, ['method'=>'PATCH', 'route'=>['user.update', $user->id]]) !!}
+						{!! Form::model($user, ['method'=>'PATCH', 'route'=>['user.update', $user->id],'files'=>true]) !!}
 						@foreach($errors->all() as $error) 
 						<p class='alert alert-danger'>{{$error}}</p>
 						@endforeach
 
-						<div class="form-group" >
-							{!! Form::label('username','Username', ['class'=>'col-md-4 control-label', 'placeholder'=>'enter the username']) !!}
-
-							{!! Form::text('username',$user->username,array('class'=>' form-control')) !!}
+						<div class="form-group">
+							{!! Form::label('foto','Avatar', ['class'=>'col-md-4 control-label']) !!}
+							
+							<div class="col-md-4">
+								<img style="margin-bottom: 5%" height="150" width="`150" class="img-circle" src="{{url('/public/assets/img/avatar/'.$user['foto'])}}">
+							</div>
+							<div class="col-md-8">
+								{!! Form::file('foto',array('class'=>'foto form-control')) !!}
+							</div>
+						</div>
+						<div class="col-md-12">
+							<hr>
 						</div>
 
 						<div class="form-group" >
-							{!! Form::label('name','Full Name', ['class'=>'col-md-4 control-label', 'placeholder'=>'Enter the name']) !!}
+							{!! Form::label('name','Name', ['class'=>'col-md-4 control-label', 'placeholder'=>'Enter the name']) !!}
 
 							{!! Form::text('name',$user->name,array('class'=>' form-control')) !!}
 						</div>
@@ -38,6 +46,38 @@
 							{!! Form::label('email','Email', ['class'=>'col-md-4 control-label']) !!}
 
 							{!! Form::text('email',$user->email,array('class'=>'email form-control','placeholder'=>'Enter the email, example: xyz@example.com')) !!}
+						</div>
+
+						<div class="form-group" >
+							{!! Form::label('motto','Motto', ['class'=>'col-md-4 control-label']) !!}
+
+							{!! Form::text('motto',$user['motto'],array('class'=>'form-control','placeholder'=>'ex: Always Be the One')) !!}
+						</div>
+
+						<div class="form-group" >
+							{!! Form::label('region','Region', ['class'=>'col-md-4 control-label']) !!}
+
+							{!! Form::text('region',$user['region'],array('class'=>'form-control','placeholder'=>'Depok')) !!}
+
+						</div>
+
+						<div class="form-group" >
+							{!! Form::label('profesi','Profession', ['class'=>'col-md-4 control-label']) !!}
+
+							{!! Form::text('profesi',$user['profesi'],array('class'=>'form-control','placeholder'=>'Student')) !!}
+						</div>
+
+						<div class="form-group" >
+							{!! Form::label('birthdate','Birthday', ['class'=>'col-md-4 control-label']) !!}
+							<br>
+							<p style="text-align: left;float: left">
+								User's Birthday = 
+								@if(!$user['birthdate'])
+								Not Define
+								@endif
+								{{$user['birthdate']}}
+							</p> 
+							<input name="birthdate" type="date" class="form-control">
 						</div>
 
 						<div class="form-group">
