5715446c52e9d6d29f6e20646b43a5a9a3186424
seryuzaki <saufi.rahman@ui.ac.id>
2016-05-15 02:09:31 +0700

    fix

--- e-recruito/app/Http/Controllers/PendaftarOprecController.php
+++ e-recruito/app/Http/Controllers/PendaftarOprecController.php
@@ -2,7 +2,7 @@
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
-
+use App\Oprec;
 use Illuminate\Http\Request;
 use App\PendaftarOprec;
 use Input, Redirect, File, Session,Validator, Response;
@@ -86,47 +86,35 @@ class PendaftarOprecController extends Controller {
 		/**
 	 *	Adding user who join to an Open Recruitment
 	 */
-<<<<<<< HEAD
 
-	public function joinOprec($iduser, $idoprec) {
-		if(session()->get('isLogin')) {
-			$oprec = Oprec::where('id',$idoprec)->first();
-			PendaftarOprec::create([
-				'iduser' => $iduser,
-				'idoprec' => $idoprec,
-			]);
-			
-			return Redirect::to('/pengguna/registered-oprec')->with('message','joined')->with('oprecName',$oprec->name);
-		} else {
-			return redirect('/');
-=======
 		public function joinOprec($iduser, $idoprec) {
 			if(session()->get('isLogin')) {
+				$oprec = Oprec::where('id',$idoprec)->first();
 				PendaftarOprec::create([
-
 					'iduser' => $iduser,
 					'idoprec' => $idoprec,
 					]);
 
-				return Redirect::to('/pengguna');
+				return Redirect::to('/pengguna/registered-oprec')->with('message','joined')->with('oprecName',$oprec->name);
+			} else {
+				return redirect('/');
+
+
+			}
+
+		}
+
+
+		public function viewAllJoinedOprec() {
+			if(session()->get('isLogin')) {
+				$user = session()->get('isLogin');
+				$title = 'Registered Oprec';
+				$allJoined = PendaftarOprec::where('iduser',$user->id)->get();
+				return view('user.registered-oprec.registered-oprec', compact('title','allJoined'));
 			} else {
 				return redirect('/');
 			}
->>>>>>> origin/Rifat
 		}
 
-<<<<<<< HEAD
-	public function viewAllJoinedOprec() {
-		if(session()->get('isLogin')) {
-			$user = session()->get('isLogin');
-			$title = 'Registered Oprec';
-			$allJoined = PendaftarOprec::where('iduser',$user->id)->get();
-			return view('user.registered-oprec.registered-oprec', compact('title','allJoined'));
-		} else {
-			return redirect('/');
-		}
+
 	}
-}
-=======
-	}
->>>>>>> origin/Rifat
--- e-recruito/resources/views/user/registered-oprec/registered-oprec.blade.php
+++ e-recruito/resources/views/user/registered-oprec/registered-oprec.blade.php
@@ -3,10 +3,10 @@
 <head>
 	@include('head')
 	<?php
-		function getOprecById($idoprec) {
-			$oprec = App\Oprec::where('id',$idoprec)->first();
-			return $oprec;
-		}
+	function getOprecById($idoprec) {
+		$oprec = App\Oprec::where('id',$idoprec)->first();
+		return $oprec;
+	}
 	?>
 </head>
 <body>
@@ -20,16 +20,16 @@
 						<hr>
 						<div class="row">
 							@if(session()->get('message') == 'joined')
-								<h5 class="aler alert-success">Congratulation! You are successfully registered on: {{$oprecName}}</h5>
+							<h5 class="aler alert-success">Congratulation! You are successfully registered on: {{session()->get('oprecName')}}</h5>
 							@endif
 						</div>
 					</div>
 					@if(count($allJoined))
 					@foreach($allJoined as $join)
-						<div class="col-md-4" style="margin-bottom:3%">
-							<img src="{{url('public/assests/img/brosur-oprec/'.$join['brosur'])}}" height="200" width="100%">
-							<h3><?php $oprec = getOprecById($join->idoprec); echo $oprec->name ?></h3>
-							<p>Days to deadline: <?php $deadline = new DateTime($oprec->deadline); 
+					<div class="col-md-4" style="margin-bottom:3%">
+						<img src="{{url('public/assests/img/brosur-oprec/'.$join['brosur'])}}" height="200" width="100%">
+						<h3><?php $oprec = getOprecById($join->idoprec); echo $oprec->name ?></h3>
+						<p>Days to deadline: <?php $deadline = new DateTime($oprec->deadline); 
 							$today = new DateTime(date("Y-m-d"));
 							$interval = $deadline->diff($today);
 							$days = $interval->format('%a');
@@ -37,16 +37,16 @@
 							echo $days." ";?></p>
 							<a href="#">View Task</a>
 						</div>
-					@endforeach
-					@else
-					<div class="aler alert-danger">Doesn't Have any registered Open Recruitment</div>
-					@endif
+						@endforeach
+						@else
+						<div class="aler alert-danger">Doesn't Have any registered Open Recruitment</div>
+						@endif
+					</div>
 				</div>
 			</div>
 		</div>
-	</div>
-</body>
-<footer>
-	@include('footer')
-</footer>
-</html>
+	</body>
+	<footer>
+		@include('footer')
+	</footer>
+	</html>
