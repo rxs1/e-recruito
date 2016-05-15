761c18de64721b82f699515c63395a6e6dc3e1db
derhif <rhief.at@gmail.com>
2016-05-15 09:16:56 +0700

    adding smthing

--- e-recruito/resources/views/user/registered-oprec/registered-oprec.blade.php
+++ e-recruito/resources/views/user/registered-oprec/registered-oprec.blade.php
@@ -3,9 +3,15 @@
 <head>
 	@include('head')
 	<?php
+	$user = session()->get('isLogin');
 	function getOprecById($idoprec) {
 		$oprec = App\Oprec::where('id',$idoprec)->first();
 		return $oprec;
+	}
+
+	function chosenField($iduser,$idoprec) {
+		$fields = App\PendaftarBidang::where('iduser',$iduser)->where('idoprec',$idoprec)->get();
+		return $fields;
 	}
 	?>
 </head>
@@ -29,6 +35,11 @@
 					<div class="col-md-4" style="margin-bottom:3%">
 						<img src="{{url('public/assests/img/brosur-oprec/'.$join['brosur'])}}" height="200" width="100%">
 						<h3><?php $oprec = getOprecById($join->idoprec); echo $oprec->name ?></h3>
+						@if(count(chosenField($user->id, $oprec->id)))
+							
+						@else
+							You have not chosen any field yet. Choose <a href="{{url('/pengguna')}}">here</a>
+						@endif
 						<p>Days to deadline: <?php $deadline = new DateTime($oprec->deadline); 
 							$today = new DateTime(date("Y-m-d"));
 							$interval = $deadline->diff($today);
